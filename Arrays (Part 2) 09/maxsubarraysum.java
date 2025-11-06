c153e389314e756b1bfb1675cb005e4f7e8145a4
Tushar Dhabale <128279576+TusharDhabale@users.noreply.github.com>
2025-11-06 22:01:45 +0530

    Add files via upload

---
+++ Arrays (Part 2) 09/maxsubarraysum.java
@@ -0,0 +1,29 @@
+public class maxsubarraysum {
+
+    public static void maxsumarray(int numbers[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int i=0; i<numbers.length; i++){
+            int start = i;
+            for (int j=i; j<numbers.length; j++){
+                int end = j;
+                currSum = 0;
+                for(int k=start; k<=end; k++){ //print
+                //sunarray sum
+                currSum += numbers[k];
+                }
+                System.out.println(currSum);
+                if(maxSum < currSum){
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max sum =  "+ maxSum);
+    }
+    //neha chudail
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        maxsumarray(numbers);
+    }
+}
